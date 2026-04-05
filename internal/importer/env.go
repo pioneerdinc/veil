882281c14821bbeca7a6e32dacef4c4b1486421f
ossydotpy <mrali.osman.6@gmail.com>
2026-04-05 10:23:53 +0000

    feat: add secret importer with env file support

---
+++ internal/importer/env.go
@@ -0,0 +1,26 @@
+package importer
+
+import (
+	"github.com/ossydotpy/veil/internal/envfile"
+	"github.com/ossydotpy/veil/internal/filter"
+)
+
+type EnvImporter struct{}
+
+func NewEnvImporter() *EnvImporter {
+	return &EnvImporter{}
+}
+
+func (e *EnvImporter) Format() string {
+	return "env"
+}
+
+func (e *EnvImporter) Import(opts ImportOptions) (map[string]string, error) {
+	secrets, err := envfile.ParseEnvFile(opts.SourcePath)
+	if err != nil {
+		return nil, err
+	}
+
+	filtered := filter.FilterSecrets(secrets, opts.Include, opts.Exclude)
+	return filtered, nil
+}
